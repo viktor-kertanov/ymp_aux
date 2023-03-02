13e754877b9fa94fc49e7498faf5d767ce264268
Viktor Kertanov <thelonius19@gmail.com>
2023-03-02 11:39:37 +0300

    Added a python module to print squares of nums. Modifed Readme with a few lines

---
+++ test_module.py
@@ -0,0 +1,9 @@
+
+def print_squares(nums):
+    for idx, num in enumerate(range(1, n+1), start=1):
+        print(f"{idx}. {num}^2={num**2}\n")
+
+
+if __name__ == '__main__':
+    n=10
+    print_squares(n)
