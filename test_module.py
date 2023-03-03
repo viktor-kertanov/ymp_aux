fbfdb939e8b6e25d141e42e5ca29947a4113afe0
Viktor Kertanov <thelonius19@gmail.com>
2023-03-03 13:12:36 +0300

    Func hello, func num to exp implemented

--- test_module.py
+++ test_module.py
@@ -1,11 +1,19 @@
 
 def print_squares(nums):
-    for idx, num in enumerate(range(1, n+1), start=1):
+    for idx, num in enumerate(range(1, nums+1), start=1):
         print(f"{idx}. {num}^2={num**2}\n")
 
 
 def two_plus_two():
     return 2+2
+
+def exp_func(i, num):
+    for el in range(i):
+        print(f"{num}^{el} = {num**el}")
+
+
 if __name__ == '__main__':
-    n=10
-    print_squares(n)
+    exp_func(5, 7)
+
+    
+    print('Hello world')
